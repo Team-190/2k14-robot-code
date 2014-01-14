dd6999c68ab861094be7b521871ec0de38e7df5d
Will Spurgeon <wespurgeon@wpi.edu>
2014-01-14 17:28:06 -0500

    Implemented the end() methods for driveWithJoysticks().

--- src/org/usfirst/frc190/CurrentRobotProject/commands/DriveWithJoysticks.java
+++ src/org/usfirst/frc190/CurrentRobotProject/commands/DriveWithJoysticks.java
@@ -38,9 +38,11 @@ public class  DriveWithJoysticks extends Command {
     }
     // Called once after isFinished returns true
     protected void end() {
+        Robot.drivetrain.tankDrive(0, 0);
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
