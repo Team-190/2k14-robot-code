1602907022cfcf5b51ea147960f4dd73a010f7f0
tucker3896 <tucker3896@tmartin.wifi.wpi.edu>
2014-01-28 10:52:48 -0500

    Done Cleaning up

--- src/org/usfirst/frc190/CurrentRobotProject/MessageLogger.java
+++ src/org/usfirst/frc190/CurrentRobotProject/MessageLogger.java
@@ -1,31 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.usfirst.frc190.CurrentRobotProject;
 
 import edu.wpi.first.wpilibj.DriverStationLCD;
 
-
-/**
- *
- * @author Will
- */
 public class MessageLogger {
-    
-    
-    public void printUserMessage(String message){
+
+    public void printUserMessage(String message) {
         DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser1, 0, message);
-        DriverStationLCD.getInstance().updateLCD(); 
+        DriverStationLCD.getInstance().updateLCD();
     }
-    
-    public void speakMessage(SpeechMessage message){
-        switch(message.value){
+
+    public void speakMessage(SpeechMessage message) {
+        switch (message.value) {
             case SpeechMessage.LEFT_VAL:
                 //TODO add code later.
                 break;
         }
 
     }
-    
 }
