cd7816a5e1e0075aab8be2bde42f4b9501e1b9fa
Will Spurgeon <wespurgeon@wpi.edu>
2014-02-09 20:17:19 -0500

    Added a comment.

--- src/org/usfirst/frc190/CurrentRobotProject/ArduinoMessages.java
+++ src/org/usfirst/frc190/CurrentRobotProject/ArduinoMessages.java
@@ -2,6 +2,10 @@ package org.usfirst.frc190.CurrentRobotProject;
 
 //This is an "enum-type" class that allows you specify how the robot should move in order to collect the ball. 
 
+//Values 0-9 are resevered for passing control commands to the Arduino.
+//Values 10-40 are reserved for pressure sensing. (Value is 10 = No lights, Value is 40 = All lights on strip on.
+//Values 41+ are reserved for different LED light strip patterns.
+
 
 public class ArduinoMessages {
     public final byte value;
