d58af000e26fb64bc5c2069830418f2426775ac8
Will Spurgeon <wespurgeon@wpi.edu>
2014-04-16 17:49:40 -0400

    Added more ArduinoMessages.

--- src/org/usfirst/frc190/CurrentRobotProject/commands/Fire.java
+++ src/org/usfirst/frc190/CurrentRobotProject/commands/Fire.java
@@ -4,12 +4,13 @@
 package org.usfirst.frc190.CurrentRobotProject.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
+import org.usfirst.frc190.CurrentRobotProject.ArduinoMessages;
 import org.usfirst.frc190.CurrentRobotProject.Robot;
 
 public class Fire extends CommandGroup {
     
     public  Fire() {
-        Robot.I2CComm.sendMessage((byte) 7);
+        Robot.I2CComm.sendMessage(ArduinoMessages.FIRE.value);
         setInterruptible(false);
         //None of the sequences for the pneumatics system need addParallel.
         //The commands take so little time there is no need.
@@ -25,7 +26,7 @@ public class Fire extends CommandGroup {
         addSequential(new RetractLatch());
         addSequential(new WaitCommand(0.25));
         addSequential(new ExtendPiston1());
-        Robot.I2CComm.sendMessage((byte) 8);
+        Robot.I2CComm.sendMessage(ArduinoMessages.BOOM_TRIPLE_THREAT.value);
         //addSequential(new ExtendPiston2());
     }
 }
